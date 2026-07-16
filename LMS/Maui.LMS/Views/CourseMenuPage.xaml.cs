45ec82ee4a350b9cdb0893378ed8dd286e2bae44
Ryan8536 <elharradaryan@gmail.com>
2026-07-16 10:53:04 -0400

    Add content to modules
    Added support for selecting a module and adding string content to that module
    
    The selected module's content is displayed and refreshed after new content is added

--- LMS/Maui.LMS/Views/CourseMenuPage.xaml.cs
+++ LMS/Maui.LMS/Views/CourseMenuPage.xaml.cs
@@ -9,6 +9,7 @@ public partial class CourseMenuPage : ContentPage
     public int CourseId { get; set; }
 
     private Course? currentCourse;
+    private Module? selectedModule;
 
     public CourseMenuPage()
     {
@@ -24,7 +25,11 @@ public partial class CourseMenuPage : ContentPage
 
         BindingContext = currentCourse;
 
+        selectedModule = null;
+        ContentEntry.Text = "";
+
         RefreshModules();
+        RefreshContent();
     }
 
     private void AddModuleClicked(
@@ -34,6 +39,36 @@ public partial class CourseMenuPage : ContentPage
         CourseServiceProxy.Current.AddModule(CourseId);
 
         RefreshModules();
+    }
+
+    private void ModuleSelectionChanged(
+        object? sender,
+        SelectionChangedEventArgs e)
+    {
+        selectedModule =
+            ModulesCollectionView.SelectedItem as Module;
+
+        RefreshContent();
+    }
+
+    private void AddContentClicked(
+        object? sender,
+        EventArgs e)
+    {
+        if (selectedModule == null)
+        {
+            return;
+        }
+
+        CourseServiceProxy.Current.AddModuleContent(
+            CourseId,
+            selectedModule.Id,
+            ContentEntry.Text
+        );
+
+        ContentEntry.Text = "";
+
+        RefreshContent();
     }
 
     private void RefreshModules()
@@ -46,6 +81,18 @@ public partial class CourseMenuPage : ContentPage
 
         ModulesCollectionView.ItemsSource =
             new List<Module>(currentCourse.Modules);
+    }
+
+    private void RefreshContent()
+    {
+        if (selectedModule == null)
+        {
+            ContentCollectionView.ItemsSource = null;
+            return;
+        }
+
+        ContentCollectionView.ItemsSource =
+            new List<string>(selectedModule.Content);
     }
 
     private async void BackClicked(
